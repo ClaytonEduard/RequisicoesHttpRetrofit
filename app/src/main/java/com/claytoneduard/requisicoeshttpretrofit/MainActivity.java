0c988430fe5815228e600c4d8fb9b5dd84a777b1
ClaytonEduard <clayton_eduard@hotmail.com>
2023-04-21 13:35:18 -0300

    Configuração Basica Retrofit

--- app/src/main/java/com/claytoneduard/requisicoeshttpretrofit/MainActivity.java
+++ app/src/main/java/com/claytoneduard/requisicoeshttpretrofit/MainActivity.java
@@ -2,13 +2,52 @@ package com.claytoneduard.requisicoeshttpretrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoRecuperar;
+    private TextView textoResultado;
+    private Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoRecuperar = findViewById(R.id.buttonRecuperar);
+        textoResultado = findViewById(R.id.textResultado);
+
+
+        botaoRecuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // configuração retrofit
+                retrofit = new Retrofit.Builder()
+                        .baseUrl("https://viacep.com.br/ws/75535210/json/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+
+            }
+        });
     }
+
 }
